2e8e8eec345e8abc80b4c791014d2c66932ae6cc
Kei <motosjairo12@gmail.com>
2024-04-30 15:40:31 +0200

    First Commit

---
+++ src/calendar/components/FabAddNew.jsx
@@ -0,0 +1,37 @@
+import { addHours } from "date-fns"
+import { useCalendarStore, useUiStore } from "../../hooks"
+import { useSelector } from "react-redux"
+
+
+export const FabAddNew = () => {
+
+    const { openDateModal } = useUiStore()
+    const { setActiveEvent } = useCalendarStore()
+    const { user } = useSelector( state => state.auth )
+
+    const handleClickNew = () => {
+        setActiveEvent({
+            // _id: new Date().getTime(),
+            title: '',
+            notes: '',
+            start: new Date(),
+            end: addHours( new Date(), 2 ),
+            bgColor: '#fafafa',
+            user: {
+                _id: '123',
+                name: 'Jairo'
+            }
+        })
+        
+        openDateModal()
+    }
+
+  return (
+    <button
+        className="btn btn-primary fab"
+        onClick={ handleClickNew }
+    >
+        <i className="fas fa-plus"></i>
+    </button>
+  )
+}
